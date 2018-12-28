136927acd6526d77aa7745e42434edb08e486bf4
RaquelCC <rcanalesconcha@gmail.com>
2018-12-27 23:09:05 -0300

    arreglado archivo json, faltaban algunas imagenes // funciones q crean tarjetas de champions y agregan graficos con chartjs funcionando

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@
 //     .then(() => console.log(championData))
 // })
 
-fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
+fetch('data/lol/lol.json')
     .then(data => data.json())
     .then (data => {
     for (let champ in data.data) {
@@ -49,3 +49,120 @@ fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
     })
     .then(() => console.log(championData))
 
+
+function showChamps (data) {
+    data.forEach(champ => {
+        document.getElementById("champ-container").innerHTML += `
+        <div class="card col s3">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href=""><img class="responsive-img img-champion" src="${champ.splash}"></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title activator">${champ.name}<i class="material-icons right">more_vert</i></span>
+                <p class="champ-title">${champ.title}</p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title">${champ.name}<i class="material-icons right">close</i></span>
+                <canvas class="champion-chart"></canvas>
+            </div>
+        </div>
+        `
+    })
+}
+
+
+
+
+
+let championGraphsCanvases = document.getElementsByClassName("champion-chart");
+let championCharts = [];
+
+function initializeCharts () {
+    for (let i = 0; i<championGraphsCanvases.length; i++) {
+        championCharts[i] = new Chart(championGraphsCanvases[i], {
+            type: 'horizontalBar',
+        data: {
+            labels: ["Attack", "Defense", "Magic", "Difficulty"],
+            datasets: [{
+                data: [championData[i].info.attack, championData[i].info.defense, championData[i].info.magic, championData[i].info.difficulty],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+        })
+    }
+}
+
+function showChampsData () {
+    showChamps(championData);
+    initializeCharts();
+}
+
+window.onload = showChampsData;
+// graficos de stats
+
+
+// var ctx = document.getElementById("myChart");
+// var myChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ["Attack", "Defense", "Magic", "Dificulty"],
+//         datasets: [{
+//             // label: '# of Votes',
+//             data: [8, 4, 3, 4],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         maintainAspectRatio: true,
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         },
+//         legend: {
+//             display: false
+//         },
+//         tooltips: {
+//             position: 'average'
+//         }
+//     }
+// });
