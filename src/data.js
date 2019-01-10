b47500671176cbfcf748c8dba95d94a8a0ca334b
RaquelCC <rcanalesconcha@gmail.com>
2019-01-10 10:47:06 -0300

    eliminados comentarios de codigo que no se va a usar, menos unos pocos para probar cosas

--- src/data.js
+++ src/data.js
@@ -1,16 +1,4 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-// const example = () => {
-//   return 'example';
-// };
-
-// window.example = example;
-
 let championData = [];
-// for (let champ in LOL.data) {
-//   championData.push(LOL.data[champ]);
-// }
 
 window.championData = championData;
 
@@ -29,7 +17,6 @@ window.championManage = {
       }
     })
     return sortedChampions;
-    // console.log(sortedChampions[0]);
   },
   filterData: (data, condition) => {  //condition es un array
     const letterFilter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -73,7 +60,6 @@ window.championManage = {
   averageStats: (data) => {
     let championStats = [];
     data.forEach(champ => {
-      // console.log(Object.keys(champ));
       Object.keys(champ.stats).forEach(key => {
         if (championStats.indexOf(key) === -1) {
           championStats.push(key);
@@ -103,15 +89,4 @@ window.championManage = {
     return foundData;
   }
 }
-//ESTO ERA PARA VER LOS RANGOS DE LOS CHAMPIONS
-// let rangos = []
-// championData.forEach(champ => {
-//   rangos.push(champ['stats']['attackrange']);
-// });
-// rangos.sort((a,b) =>{
-//   return (Number(a) > Number(b) ? 1 : -1);
-// })
-// console.log(rangos);
 
-// console.log(championManage.sortData(championData, 'key', 'ascending')[0]);
-// console.log(championManage.sortData(championData, 'key', 'ascending')[133]);
